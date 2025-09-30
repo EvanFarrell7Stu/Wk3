a958e7ba0e33fa0050ae89488d76e86d6c4ef392
Evan Farrell <g00326940@atu.ie>
2025-09-30 22:13:04 +0100

    Added add method

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,10 @@
+package ie.atu;
+
+public class Calculator
+{
+    public int add(int FirstNumber, int SecondNumber) {
+
+        return FirstNumber + SecondNumber;
+    }
+    //Adds two numbers
+}
